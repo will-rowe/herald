03fe8fa690f6f48e838168fda0bfc41600baa1e4
Will Rowe <w.p.m.rowe@gmail.com>
2020-03-02 16:16:16 +0000

    dir check restricted to dirs only - was allowing files too

--- src/helpers/helpers.go
+++ src/helpers/helpers.go
@@ -8,12 +8,13 @@ import (
 
 // CheckDirExists is used to check a directory exists
 func CheckDirExists(dirPath string) error {
-	if _, err := os.Stat(dirPath); err != nil {
-		if os.IsNotExist(err) {
-			return fmt.Errorf("directory does not exist (%v)", dirPath)
-		} else {
-			return nil
-		}
+	fh, err := os.Stat(dirPath)
+	switch {
+	case err != nil:
+		return err
+	case fh.IsDir():
+		return nil
+	default:
+		return fmt.Errorf("not a directory: %v", dirPath)
 	}
-	return nil
 }
